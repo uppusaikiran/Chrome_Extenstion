909532389e62cd8e67957a15d6aea209d01b041c
uppusaikiran <saikiran.vit@gmail.com>
2016-06-04 22:58:00 +0530

    added each item id extraction

--- answer_collapse/content.js
+++ answer_collapse/content.js
@@ -4,9 +4,16 @@ var answer = $(".feed_type_answer");
 //console.log(answer);
 
 answers = []
+
 function pull() {
-  var answers = $(".feed_type_answer");
-  console.log(answers.length);
+    var answers = $(".feed_type_answer");
+    //console.log(answers.length);
+    for (var i = 0; i < answers.length; i++) {
+      answers[i].addEventListener("dblclick",listnerFunc)
+    }
+}
+function listnerFunc() {
+  alert(this.id);
 }
 
-var answerPolls = setInterval(pull , 2000);
+var answerPolls = setInterval(pull, 2000);
