eedeaf35cb45bb31b9e1aa0f7ec10417b096b1de
uppusaikiran <saikiran.vit@gmail.com>
2016-06-04 23:22:09 +0530

    bug fix dblclick works

--- answer_collapse/content.js
+++ answer_collapse/content.js
@@ -1,19 +1,29 @@
 //This message only shows in quora website
 console.log("Hello quora");
-var answer = $(".feed_type_answer");
+//var answer = $(".feed_type_answer");
 //console.log(answer);
 
-answers = []
 
-function pull() {
-    var answers = $(".feed_type_answer");
-    //console.log(answers.length);
-    for (var i = 0; i < answers.length; i++) {
-      answers[i].addEventListener("dblclick",listnerFunc)
+$(document).ready(function() {
+
+
+    var answers = [];
+    var answerlen = answers.length;
+
+    function pull() {
+        answers = $(".feed_type_answer");
+        //console.log(answers.length);
+        for (var i = answerlen; i < answers.length; i++) {
+            answers[i].addEventListener("dblclick", listnerFunc)
+        }
+        answerlen = answers.length;
     }
-}
-function listnerFunc() {
-  alert(this.id);
-}
 
-var answerPolls = setInterval(pull, 2000);
+    function listnerFunc() {
+        var store = $(".Expandable.SimpleToggle.Toggle.AnswerInFeedExpandable.AnswerExpandable");
+        $("#" + store[0].id).removeClass("hidden");
+        $("#" + store[1].id).addClass("hidden");
+    }
+
+    var answerPolls = setInterval(pull, 2000);
+});
