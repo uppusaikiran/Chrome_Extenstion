6d88f9c9d9ee06f4b8fb076ba1a4984bdd3df72d
uppusaikiran <saikiran.vit@gmail.com>
2016-06-04 22:27:49 +0530

    added functionality to get answers per 2 seconds

--- answer_collapse/content.js
+++ answer_collapse/content.js
@@ -1,2 +1,12 @@
 //This message only shows in quora website
 console.log("Hello quora");
+var answer = $(".feed_type_answer");
+//console.log(answer);
+
+answers = []
+function pull() {
+  var answers = $(".feed_type_answer");
+  console.log(answers.length);
+}
+
+var answerPolls = setInterval(pull , 2000);
